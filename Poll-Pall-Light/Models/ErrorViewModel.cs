d7794567ab0ebc3047bfd824cead579aac163785
Wouters Ben <woutersbenj@gmail.com>
2021-06-10 11:19:00 +0200

    first commit

---
+++ Poll-Pall-Light/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Poll_Pall_Light.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
