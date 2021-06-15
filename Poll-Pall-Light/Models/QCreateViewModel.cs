7287f32d5e3e9070b5bb8a519d04badcdcadc35c
Strazil001 <strazil001@gmail.com>
2021-06-15 11:18:13 +0200

    Added jquery test page under privay.

--- Poll-Pall-Light/Models/QCreateViewModel.cs
+++ Poll-Pall-Light/Models/QCreateViewModel.cs
@@ -2,6 +2,7 @@ namespace Poll_Pall_Light.Models
 {
     public class QCreateViewModel 
     {
+        public int? PollID { get; set; }
         public string Title { get; set; }
     }
 }
--- QItemAPI/Services/IQService.cs
+++ QItemAPI/Services/IQService.cs
@@ -4,9 +4,11 @@ using QItemAPI.Models;
 
 namespace QItemAPI.Services {
     public interface IQService {
-        Task<List<QItem>> GetAItems();
-        Task<QItem> GetAItemByID(int? id);
-        void AddAItem(QItem qItem);
-        void DeleteAItem(int? id);
+        Task<List<QItem>> GetQItems();
+        Task<List<QItem>> GetQItemsByPollId(int? id);
+        Task<QItem> GetQItemByID(int? id);
+        void AddQItem(QItem qItem);
+        void UpdateQItem(QItem qItem);
+        void DeleteQItem(int? id);
     }
 }
