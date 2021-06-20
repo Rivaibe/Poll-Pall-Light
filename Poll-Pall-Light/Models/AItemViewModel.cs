fba361b1caa66dd8d86600831ca1ee2e79f321b1
Strazil001 <strazil001@gmail.com>
2021-06-20 16:35:13 +0200

    A variable try out

--- Poll-Pall-Light/Models/AItemViewModel.cs
+++ Poll-Pall-Light/Models/AItemViewModel.cs
@@ -2,6 +2,8 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Web.Mvc;
 using AItemAPI.Models;
+using Microsoft.AspNetCore.Http;
+using PollAPI.Models;
 using QItemAPI.Models;
 using SelectListItem=Microsoft.AspNetCore.Mvc.Rendering.SelectListItem;
 
@@ -16,5 +18,12 @@ namespace Poll_Pall_Light.Models
         public List<AItem> AItems { get; set; }
         public string QId { get; set; }
         public List<QItem> QConnectedByPollId { get; set; }
+        public List<PollVariables> VariablesList { get; set; }
+        public PollVariables PollVariables { get; set; }
+        public bool QText { get; set; }
+        public bool QImage { get; set; }
+        public bool QBool { get; set; }
+        public bool QNumber { get; set; }
+        public IFormFile Image { get; set; } 
     }
 }
